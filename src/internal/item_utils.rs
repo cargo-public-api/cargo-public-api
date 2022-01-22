aba3cc89f1220a2f6b072d3aba8464bf64b4067d
Martin Nordholts <enselic@gmail.com>
2022-01-22 14:04:49 +0100

    Add TODO for when rust-lang/rust issue 92945 is fixed

--- src/internal/item_utils.rs
+++ src/internal/item_utils.rs
@@ -11,6 +11,7 @@ pub fn contained_items_in_item(item: &Item) -> Option<&Vec<Id>> {
         ItemEnum::Trait(t) => Some(&t.items),
         ItemEnum::Impl(i) => Some(&i.items),
         ItemEnum::Variant(rustdoc_types::Variant::Struct(ids)) => Some(ids),
+        // TODO: `ItemEnum::Variant(rustdoc_types::Variant::Tuple(ids)) => Some(ids),` when https://github.com/rust-lang/rust/issues/92945 is fixed
         _ => None,
     }
 }

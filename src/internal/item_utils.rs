12bb4837310aa7654f7646c58ea86bad3064e2d5
Martin Nordholts <enselic@gmail.com>
2022-01-22 14:04:18 +0100

    Introduce item_utils mod

---
+++ src/internal/item_utils.rs
@@ -0,0 +1,16 @@
+use rustdoc_types::{Id, Item, ItemEnum};
+
+/// Some items contain other items, which is relevant for analysis. Keep track
+/// of such relationships.
+pub fn contained_items_in_item(item: &Item) -> Option<&Vec<Id>> {
+    match &item.inner {
+        ItemEnum::Module(m) => Some(&m.items),
+        ItemEnum::Union(u) => Some(&u.fields),
+        ItemEnum::Struct(s) => Some(&s.fields),
+        ItemEnum::Enum(e) => Some(&e.variants),
+        ItemEnum::Trait(t) => Some(&t.items),
+        ItemEnum::Impl(i) => Some(&i.items),
+        ItemEnum::Variant(rustdoc_types::Variant::Struct(ids)) => Some(ids),
+        _ => None,
+    }
+}

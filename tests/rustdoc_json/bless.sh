4b007b66619f181b56442f8143dbd277f6a50061
Martin Nordholts <enselic@gmail.com>
2022-02-20 10:27:26 +0100

    tests/rustdoc_json/bless.sh: Clarify absence of trailing newline

--- tests/rustdoc_json/bless.sh
+++ tests/rustdoc_json/bless.sh
@@ -10,5 +10,7 @@ crates="
 "
 
 for crate in ${crates}; do
+    # Note that we must remove the trailing newline, otherwise `.split('\n')`
+    # will yield an empty last item
     printf "%s" "$(cargo run ${base}/${crate}_FORMAT_VERSION_10.json)" > "${base}/${crate}-expected.txt"
 done

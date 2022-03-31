137e28f30ef657dc89a2216e092c29feb3b84c49
Douwe Schulte <d.schulte@uu.nl>
2022-03-31 17:21:02 +0200

    Updated colouring to use the flat tokens

--- src/markdown.rs
+++ src/markdown.rs
@@ -52,188 +52,28 @@ fn print_items_with_header<W: std::io::Write, T>(
     writeln!(w)
 }
 
-use ansi_term::{ANSIString, ANSIStrings, Colour, Style};
+use ansi_term::{ANSIString, ANSIStrings, Colour};
 use public_items::tokens::*;
 
 fn print_with_colour(item: &public_items::PublicItem) -> String {
-    if let Ok(tokens) = item.tokens() {
-        let mut output = Vec::new();
-        let space = Style::new().paint(" ");
-
-        for qual in &tokens.qualifiers {
-            output.push(Colour::Blue.paint(qual.to_string()));
-            output.push(space.clone());
-        }
-
-        let mut path = Vec::new();
-        for id in &tokens.path {
-            path.push(Colour::Blue.paint(id));
-            path.push(Style::new().paint("::"));
-        }
-        path.remove(path.len() - 1);
-
-        match &tokens.kind {
-            Kind::Function {
-                generics,
-                arguments,
-                return_type,
-            } => {
-                output.push(Style::new().paint("fn "));
-                output.extend(path);
-                if !generics.params.is_empty() {
-                    output.push(Style::new().paint("<"));
-                    for param in &generics.params {
-                        output.push(Colour::Blue.paint(param.name.clone()));
-                        output.push(Style::new().paint(": "));
-                        output.push(Colour::Red.paint("generic stuff")); // TODO: add better support
-                        output.push(Style::new().paint(","));
-                    }
-                    output.remove(output.len() - 1);
-                    output.push(Style::new().paint(">"));
-                }
-                output.push(Style::new().paint("("));
-                for (name, ty) in arguments {
-                    output.push(Style::new().dimmed().paint(name));
-                    output.push(Style::new().paint(": "));
-                    output.extend(colour_type(ty));
-                    output.push(Style::new().paint(","));
-                }
-                if !arguments.is_empty() {
-                    output.remove(output.len() - 1);
-                }
-                output.push(Style::new().paint(")"));
-                if let Some(ty) = return_type {
-                    output.push(Style::new().paint(" -> "));
-                    output.extend(colour_type(ty));
-                }
-            }
-            Kind::Enum => {
-                output.push(Style::new().paint("enum "));
-                output.extend(path);
-            }
-            Kind::EnumVariant(var) => {
-                output.push(Style::new().paint("enum variant "));
-                output.extend(path);
-
-                match var {
-                    Variant::Plain => {}
-                    Variant::Tuple(types) => output.extend(colour_tuple(types)),
-                    Variant::Struct(ids) => {
-                        output.push(Style::new().paint("{"));
-                        for id in ids {
-                            output.push(Colour::Green.paint(id.0.clone()));
-                            output.push(Style::new().paint(", "));
-                        }
-                        output.remove(output.len() - 1);
-                        output.push(Style::new().paint("}"));
-                    }
-                }
-            }
-            Kind::Struct => {
-                output.push(Style::new().paint("struct "));
-                output.extend(path);
-            }
-            Kind::StructField(ty) => {
-                output.push(Style::new().paint("struct field "));
-                output.extend(path);
-                output.push(Style::new().paint(": "));
-                output.extend(colour_type(ty));
-            }
-        }
-
-        ANSIStrings(&output).to_string()
+    if let Some(tokens) = item.tokens() {
+        let styled = tokens.tokens().map(colour_token).collect::<Vec<_>>();
+        ANSIStrings(&styled).to_string()
     } else {
         format!("{}", item)
     }
 }
 
-fn colour_type(ty: &Type) -> Vec<ANSIString<'_>> {
-    match ty {
-        Type::ResolvedPath { name, .. } => vec![Colour::Yellow.paint(name)],
-        Type::Generic(name) => vec![Colour::Green.bold().paint(name)],
-        Type::Primitive(name) => vec![Colour::Green.paint(name)],
-        Type::FunctionPointer(_) => vec![Colour::Red.paint("Function pointer")], // TODO: add something better
-        Type::Tuple(types) => colour_tuple(types),
-        Type::Slice(ty) => {
-            let mut output = vec![Style::new().paint("[")];
-            output.extend(colour_type(ty));
-            output.push(Style::new().paint("]"));
-            output
-        }
-        Type::Array { type_, len } => {
-            let mut output = vec![Style::new().paint("[")];
-            output.extend(colour_type(type_));
-            output.push(Style::new().paint(": "));
-            output.push(Colour::Green.bold().paint(len));
-            output.push(Style::new().paint("]"));
-            output
-        }
-        Type::ImplTrait(bounds) => {
-            let mut output = vec![Style::new().paint("impl ")];
-            for bound in bounds {
-                if let GenericBound::TraitBound { trait_, .. } = bound {
-                    output.extend(colour_type(trait_));
-                    output.push(Style::new().paint(" + "));
-                }
-            }
-            output.remove(output.len() - 1);
-            output
-        }
-        Type::Infer => vec![Style::new().paint("_")],
-        Type::RawPointer { mutable, type_ } => {
-            let mut output = vec![Style::new().paint("*")];
-            if *mutable {
-                output.push(Colour::Blue.paint("mut"));
-            }
-            output.push(Style::new().paint(" "));
-            output.extend(colour_type(type_));
-            output
-        }
-        Type::BorrowedRef {
-            lifetime,
-            mutable,
-            type_,
-        } => {
-            let mut output = vec![Style::new().paint("&")];
-            if let Some(lt) = lifetime {
-                output.push(Colour::Yellow.paint(lt));
-                output.push(Style::new().paint(" "));
-            }
-            if *mutable {
-                output.push(Colour::Blue.paint("mut"));
-                output.push(Style::new().paint(" "));
-            }
-            output.extend(colour_type(type_));
-            output
-        }
-        Type::QualifiedPath {
-            name,
-            args: _, // TODO: check if this output if correct
-            self_type,
-            trait_,
-        } => {
-            let mut output = vec![Style::new().paint("<")];
-            output.extend(colour_type(self_type));
-            output.push(Style::new().paint(" as "));
-            output.extend(colour_type(trait_));
-            output.push(Style::new().paint(">::"));
-            output.push(Style::new().paint(name));
-            output
-        }
+fn colour_token(token: &Token) -> ANSIString<'_> {
+    match token {
+        Token::Symbol(text) => Colour::White.paint(text),
+        Token::Qualifier(text) => Colour::Blue.paint(text),
+        Token::Kind(text) => Colour::Blue.paint(text),
+        Token::Whitespace => Colour::White.paint(" "),
+        Token::Identifier(text) => Colour::Cyan.paint(text),
+        Token::Keyword(text) => Colour::Purple.paint(text),
+        Token::Generic(text) => Colour::Green.bold().paint(text),
+        Token::Primitive(text) => Colour::Yellow.paint(text),
+        Token::Type(text) => Colour::Green.paint(text),
     }
-}
-
-fn colour_tuple(types: &[Type]) -> Vec<ANSIString<'_>> {
-    let mut output = vec![Style::new().paint("(")];
-    for ty in types {
-        output.extend(colour_type(ty));
-        output.push(Style::new().paint(", "));
-    }
-    if !types.is_empty() {
-        output.remove(output.len() - 1);
-    } else {
-        output.push(Style::new().paint(","));
-    }
-    output.push(Style::new().paint(")"));
-    output
 }

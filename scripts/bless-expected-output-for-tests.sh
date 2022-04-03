9706e576fc1495ea5570ab22f37100491ad4f85b
Martin Nordholts <enselic@gmail.com>
2022-04-03 16:43:22 +0200

    Make bless-expected-output-for-tests.sh work again
    And after
    https://github.com/Enselic/public_items/commit/aee94349f2a8c30114f5aacd7fc3e1a1ee10d351
    we don't need to struggle to remove the trailing newline any longer.

--- scripts/bless-expected-output-for-tests.sh
+++ scripts/bless-expected-output-for-tests.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
-base="tests/rustdoc_json"
+src="tests/rustdoc_json"
+dst="tests/expected_output"
 
-source "${base}/test-crates-and-versions.sh"
+source "scripts/test-crates-and-versions.sh"
 
 for crate in ${crates}; do
-    # Note that we must remove the trailing newline, otherwise `.split('\n')`
-    # will yield an empty last item
-    printf "%s" "$(cargo run -- ${base}/${crate}.json)" > "${base}/${crate}-expected.txt"
+    cargo run -- "${src}/${crate}.json" > "${dst}/${crate}.txt"
 done
 
-printf "%s" "$(cargo run -- --with-blanket-implementations ${base}/public_items-v0.4.0.json)" > "${base}/public_items-v0.4.0-expected-with-blanket-implementations.txt"
+cargo run -- --with-blanket-implementations "${src}/public_items-v0.4.0.json" > "${dst}/public_items-v0.4.0-with-blanket-implementations.txt"

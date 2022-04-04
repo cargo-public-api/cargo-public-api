c2bc0ef3b760872735141d7a8492072f4fe92494
Martin Nordholts <enselic@gmail.com>
2022-04-04 18:00:58 +0200

    comprehensive_api: Add enums and typedefs

--- tests/crates/comprehensive_api/src/lib.rs
+++ tests/crates/comprehensive_api/src/lib.rs
@@ -8,10 +8,12 @@ pub use structs::Plain as RenamedPlain;
 mod private;
 
 pub mod constants;
+pub mod enums;
 pub mod functions;
 pub mod impls;
 pub mod macros;
 pub mod statics;
 pub mod structs;
 pub mod traits;
+pub mod typedefs;
 pub mod unions;

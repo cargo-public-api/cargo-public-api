a0419b839fb9667c335d00dce57f1c4c32138f7f
Martin Nordholts <enselic@gmail.com>
2022-04-08 21:22:27 +0200

    Point cargo-public-items to cargo-public-api

--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-/// For self-testing purposes. Please ignore.
-pub fn for_self_testing_purposes_please_ignore() {
-    println!("If you are looking for the library this tool uses, it can be found at https://github.com/Enselic/public_items");
-}

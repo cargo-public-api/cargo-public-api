70ae8adb6646f2c8b60addb1cc24ce97e1bc7cf0
Martin Nordholts <enselic@gmail.com>
2022-04-09 16:26:39 +0200

    README: Mention MINIMUM_RUSTDOC_JSON_VERSION in installation instructions
    And add a test so we don't forget to update the instruction when we bump
    MINIMUM_RUSTDOC_JSON_VERSION next time.

---
+++ tests/readme_tests.rs
@@ -0,0 +1,9 @@
+use public_items::MINIMUM_RUSTDOC_JSON_VERSION;
+
+#[test]
+fn installation_instructions_mentions_minimum_rustdoc_json_version() {
+    let readme = include_str!("../README.md");
+    let expected_installation_instruction =
+        format!("# Install {} or later", MINIMUM_RUSTDOC_JSON_VERSION);
+    assert!(readme.contains(&expected_installation_instruction));
+}

792c65e14535be5c229671f60fdb969cd3a99bde
Fraser Hutchison <fraser@casperlabs.io>
2022-05-31 03:41:20 +0100

    Highlight changes in coloured diff output

--- cargo-public-api/src/plain.rs
+++ cargo-public-api/src/plain.rs
@@ -1,9 +1,8 @@
 use std::io::{Result, Write};
 
-use public_api::{diff::PublicItemsDiff, PublicItem};
-
 use ansi_term::{ANSIString, ANSIStrings, Color, Style};
-use public_api::tokens::Token;
+use itertools::Itertools;
+use public_api::{diff::PublicItemsDiff, tokens::Token, PublicItem};
 
 use crate::{
     output_formatter::{print_items_with_header, OutputFormatter},
@@ -49,11 +48,14 @@ impl OutputFormatter for Plain {
             &diff.changed,
             |w, changed_item| {
                 if use_color {
+                    let diff_chars =
+                        diff::chars(&changed_item.old.to_string(), &changed_item.new.to_string());
+
                     writeln!(
                         w,
                         "-{}\n+{}",
-                        color_item(&changed_item.old),
-                        color_item(&changed_item.new)
+                        color_item_with_diff(&changed_item.old, &diff_chars, true),
+                        color_item_with_diff(&changed_item.new, &diff_chars, false),
                     )
                 } else {
                     writeln!(w, "-{}\n+{}", changed_item.old, changed_item.new)
@@ -115,4 +117,61 @@ fn color_item_token(token: &Token, bg: Option<Color>) -> ANSIString<'_> {
         Token::Primitive(text) => style(Color::Green.into(), text),
         Token::Type(text) => style(Color::Green.into(), text),
     }
+}
+
+fn color_item_with_diff(
+    item: &PublicItem,
+    diff_chars: &[diff::Result<char>],
+    is_old_item: bool,
+) -> String {
+    let diff_style = if is_old_item {
+        Color::Fixed(9).on(Color::Fixed(52)).bold()
+    } else {
+        Color::Fixed(10).on(Color::Fixed(22)).bold()
+    };
+
+    // Create a series of batches of `Some(style)` or `None` depending on whether each given char
+    // should be styled as a diff.
+    let mut diff_sequences = vec![];
+    diff_chars.iter().for_each(|result| match result {
+        diff::Result::Left(_) => {
+            if is_old_item {
+                diff_sequences.push(Some(diff_style));
+            }
+        }
+        diff::Result::Both(..) => {
+            diff_sequences.push(None);
+        }
+        diff::Result::Right(_) => {
+            if !is_old_item {
+                diff_sequences.push(Some(diff_style));
+            }
+        }
+    });
+
+    // Create the default coloured strings for this collection of tokens.
+    let default_coloured_strings = item
+        .tokens()
+        .map(|t| color_item_token(t, None))
+        .collect::<Vec<_>>();
+    let ansi_strings = ANSIStrings(&default_coloured_strings);
+
+    // Collect the modified and unmodified substrings.
+    let mut diff_strings = vec![];
+    let mut seq_start_index = 0;
+    // Turn the batches of optional styles into an iterator of tuples of (num of consecutive
+    // identical, optional style).
+    for (len, maybe_style) in diff_sequences.into_iter().dedup_with_count() {
+        let mut sub_strings = ansi_term::sub_string(seq_start_index, len, &ansi_strings);
+        // If this batch of chars should have a diff style, apply it to the sub-strings.
+        if let Some(style) = maybe_style {
+            for sub_string in &mut sub_strings {
+                *sub_string.style_ref_mut() = style;
+            }
+        }
+        diff_strings.extend(sub_strings);
+        seq_start_index += len;
+    }
+
+    ANSIStrings(&diff_strings).to_string()
 }

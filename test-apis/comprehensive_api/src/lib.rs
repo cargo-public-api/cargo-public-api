3bcb28c86d529a0e8b34dd0f41dd89ec85c2c139
Martin Nordholts <enselic@gmail.com>
2022-07-21 22:02:41 +0200

    comprehensive_api/src/lib.rs: Better grouping

--- test-apis/comprehensive_api/src/lib.rs
+++ test-apis/comprehensive_api/src/lib.rs
@@ -1,23 +1,33 @@
 pub extern crate rand;
-
-pub use private::StructInPrivateMod;
 pub use rand::RngCore;
+
+mod private;
+pub use private::StructInPrivateMod;
+
+pub mod attributes;
+
+pub mod constants;
+
+pub mod enums;
+
+pub mod functions;
+
+pub mod higher_ranked_trait_bounds;
+
+pub mod impls;
+
+pub mod macros;
+
+pub mod statics;
+
+pub mod structs;
 pub use structs::Plain;
 pub use structs::Plain as RenamedPlain;
 
-mod private;
-
-pub mod attributes;
-pub mod constants;
-pub mod enums;
-pub mod functions;
-pub mod higher_ranked_trait_bounds;
-pub mod impls;
-pub mod macros;
-pub mod statics;
-pub mod structs;
 pub mod traits;
+
 pub mod typedefs;
+
 pub mod unions;
 
 pub use u32;

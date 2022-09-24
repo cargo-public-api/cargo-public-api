cargo-public-api/cargo-public-api

commit 4a1ad459e6db10468f1e19c594bb876da63926d8

Always fully qualify paths to structs, enums, unions, etc when possible (#156)
* Remember if an item was renamed

So that this can be used in future heuristics.

* Fully qualify paths to types in args and fields etc

This removes ambiguity and also prepares for us rendering `impl`s, in
which case it is even more important to remove ambiguities.

* Make `--document-private-items` pass

Co-authored-by: Emil Gardström <emil.gardstrom@gmail.com>

* Make `IntermediatePublicItem::name()` return reference

* Use `max_by()` in `best_item_for_id()` and fix typo

* Remove accidentallly commited `// HTML difference!` comment

Co-authored-by: Emil Gardström <emil.gardstrom@gmail.com>

public-api/src/render.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::unused_self)]
 use crate::intermediate_public_item::IntermediatePublicItem;
-use std::rc::Rc;
+use std::{cmp::Ordering, collections::HashMap, rc::Rc};
 
 use rustdoc_types::{
     Abi, Constant, Crate, FnDecl, FunctionPointer, GenericArg, GenericArgs, GenericBound,
@@ -17,8 +17,16 @@ macro_rules! ws {
 
 use crate::tokens::Token;
 
+/// When we render an item, it might contain references to other parts of the
+/// public API. For such cases, the rendering code can use the fields in this
+/// struct.
 pub struct RenderingContext<'a> {
+    /// The original (deserialized) rustdoc JSON.
     pub crate_: &'a Crate,
+
+    /// Given a rustdoc JSON ID, keeps track of what public items that have this
+    /// ID. See [`crate::item_iterator::ItemIterator::id_to_items`] for more info.
+    pub id_to_items: HashMap<&'a Id, Vec<Rc<IntermediatePublicItem<'a>>>>,
 }
 
 impl<'a> RenderingContext<'a> {
@@ -228,7 +236,7 @@ impl<'a> RenderingContext<'a> {
             } else {
                 Token::identifier
             };
-            output.push(token_fn(item.name.clone()));
+            output.push(token_fn(item.name()));
             output.push(Token::symbol("::"));
         }
         if !path.is_empty() {
@@ -456,7 +464,9 @@ impl<'a> RenderingContext<'a> {
     fn render_resolved_path(&self, path: &Path) -> Vec<Token> {
         let mut output = vec![];
         let name = &path.name;
-        if !name.is_empty() {
+        if let Some(item) = self.best_item_for_id(&path.id) {
+            output.extend(self.render_path(&item.path()));
+        } else if !name.is_empty() {
             output.extend(self.render_path_name(name));
             if let Some(args) = &path.args {
                 output.extend(self.render_generic_args(args));
@@ -807,6 +817,38 @@ impl<'a> RenderingContext<'a> {
             output.push(ws!());
         }
         output
+    }
+
+    fn best_item_for_id(&self, id: &Id) -> Option<Rc<IntermediatePublicItem<'a>>> {
+        match self.id_to_items.get(&id) {
+            None => None,
+            Some(items) => {
+                items
+                    .iter()
+                    .max_by(|a, b| {
+                        // If there is any item in the path that has been
+                        // renamed/re-exported, i.e. that is not the original
+                        // path, prefer that less than an item with a path where
+                        // all items are original.
+                        let mut ordering = match (
+                            a.path_contains_renamed_item(),
+                            b.path_contains_renamed_item(),
+                        ) {
+                            (true, false) => Ordering::Less,
+                            (false, true) => Ordering::Greater,
+                            _ => Ordering::Equal,
+                        };
+
+                        // If we still can't make up our mind, go with the shortest path
+                        if ordering == Ordering::Equal {
+                            ordering = b.path().len().cmp(&a.path().len());
+                        }
+
+                        ordering
+                    })
+                    .cloned()
+            }
+        }
     }
 }
 
@@ -1188,7 +1230,10 @@ mod test {
             external_crates: HashMap::new(),
             format_version: 0,
         };
-        let context = RenderingContext { crate_: &crate_ };
+        let context = RenderingContext {
+            crate_: &crate_,
+            id_to_items: HashMap::new(),
+        };
 
         let actual = render_fn(context);
 

cargo-public-api/tests/cargo-public-api-bin-tests.rs
@@ -311,7 +311,7 @@ fn deny_removed_with_diff() {
     cmd.arg("--deny=removed");
     cmd.assert()
         .stderr(contains(
-            "The API diff is not allowed as per --deny: Removed items not allowed: [pub fn example_api::function(v1_param: Struct, v2_param: usize)]",
+            "The API diff is not allowed as per --deny: Removed items not allowed: [pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize)]",
         ))
         .failure();
 }

public-api/src/intermediate_public_item.rs
@@ -13,9 +13,9 @@ pub struct IntermediatePublicItem<'a> {
     /// The item we are effectively wrapping.
     pub item: &'a Item,
 
-    /// The name of the item. Normally this is [Item::name]. But in the case of
-    /// renamed imports (`pub use other::item as foo;`) it is the new name.
-    pub name: String,
+    /// If `Some`, this overrides [Item::name], which happens in the case of
+    /// renamed imports (`pub use other::Item as Foo;`).
+    pub overridden_name: Option<String>,
 
     /// The parent item. If [Self::item] is e.g. an enum variant, then the
     /// parent is an enum. We follow the chain of parents to be able to know the
@@ -27,10 +27,21 @@ impl<'a> IntermediatePublicItem<'a> {
     #[must_use]
     pub fn new(
         item: &'a Item,
-        name: String,
+        overridden_name: Option<String>,
         parent: Option<Rc<IntermediatePublicItem<'a>>>,
     ) -> Self {
-        Self { item, name, parent }
+        Self {
+            item,
+            overridden_name,
+            parent,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        self.overridden_name
+            .as_deref()
+            .or(self.item.name.as_deref())
+            .unwrap_or("<<no_name>>")
     }
 
     #[must_use]
@@ -53,6 +64,11 @@ impl<'a> IntermediatePublicItem<'a> {
     #[must_use]
     pub fn path_contains_id(&self, id: &'a Id) -> bool {
         self.path().iter().any(|m| m.item.id == *id)
+    }
+
+    #[must_use]
+    pub fn path_contains_renamed_item(&self) -> bool {
+        self.path().iter().any(|m| m.overridden_name.is_some())
     }
 
     pub fn render_token_stream(&self, context: &RenderingContext) -> Vec<Token> {

public-api/src/item_iterator.rs
@@ -42,6 +42,16 @@ pub struct ItemIterator<'a> {
     /// What items left to visit (and possibly add more items from)
     items_left: Vec<Rc<IntermediatePublicItem<'a>>>,
 
+    /// Given a rustdoc JSON ID, keeps track of what public items that have this
+    /// ID. The reason this is a one-to-many mapping is because of re-exports.
+    /// If an API re-exports a public item in a different place, the same item
+    /// will be reachable by different paths, and thus the Vec will contain many
+    /// [`IntermediatePublicItem`]s for that ID.
+    ///
+    /// You might think this is rare, but it is actually more common than you
+    /// think out in the wild.
+    id_to_items: HashMap<&'a Id, Vec<Rc<IntermediatePublicItem<'a>>>>,
+
     /// Normally, an item referenced by item Id is present in the rustdoc JSON.
     /// If [`Self::crate_.index`] is missing an Id, then we add it here, to aid
     /// with debugging. It will typically be missing because of bugs (or
@@ -70,6 +80,7 @@ impl<'a> ItemIterator<'a> {
         let mut s = ItemIterator {
             crate_,
             items_left: vec![],
+            id_to_items: HashMap::new(),
             missing_ids: vec![],
             active_impls: active_impls(all_impls.clone(), options),
         };
@@ -163,7 +174,7 @@ impl<'a> ItemIterator<'a> {
         mut item: &'a Item,
         parent: Option<Rc<IntermediatePublicItem<'a>>>,
     ) {
-        let mut name = item.name.clone();
+        let mut overridden_name = None;
 
         // Since public imports are part of the public API, we inline them, i.e.
         // replace the item corresponding to an import with the item that is
@@ -174,7 +185,7 @@ impl<'a> ItemIterator<'a> {
         // primitive types, there is no item Id to inline with, so they remain
         // as e.g. `pub use my_i32` in the output.
         if let ItemEnum::Import(import) = &item.inner {
-            name = if import.glob {
+            overridden_name = if import.glob {
                 // Items should have been inlined in maybe_add_item_to_visit(),
                 // but since we got here that must have failed, typically
                 // because the built rustdoc JSON omitted some items from the
@@ -196,12 +207,12 @@ impl<'a> ItemIterator<'a> {
             };
         }
 
-        let public_item = Rc::new(IntermediatePublicItem::new(
-            item,
-            name.unwrap_or_else(|| String::from("<<no_name>>")),
-            parent,
-        ));
+        let public_item = Rc::new(IntermediatePublicItem::new(item, overridden_name, parent));
 
+        self.id_to_items
+            .entry(&item.id)
+            .or_default()
+            .push(public_item.clone());
         self.items_left.push(public_item);
     }
 
@@ -294,15 +305,19 @@ fn items_in_container(item: &Item) -> Option<&Vec<Id>> {
 }
 
 pub fn public_api_in_crate(crate_: &Crate, options: Options) -> super::PublicApi {
-    let context = RenderingContext { crate_ };
     let mut item_iterator = ItemIterator::new(crate_, options);
-    let items = item_iterator
-        .by_ref()
-        .map(|p| intermediate_public_item_to_public_item(&context, &p))
-        .collect();
+    let items: Vec<_> = item_iterator.by_ref().collect();
+
+    let context = RenderingContext {
+        crate_,
+        id_to_items: item_iterator.id_to_items,
+    };
 
     PublicApi {
-        items,
+        items: items
+            .iter()
+            .map(|item| intermediate_public_item_to_public_item(&context, item))
+            .collect(),
         missing_item_ids: item_iterator
             .missing_ids
             .iter()
@@ -319,7 +334,7 @@ fn intermediate_public_item_to_public_item(
         path: public_item
             .path()
             .iter()
-            .map(|i| i.name.clone())
+            .map(|i| i.name().to_owned())
             .collect::<PublicItemPath>(),
         tokens: public_item.render_token_stream(context),
     }

public-api/tests/public-api-bin-tests.rs
@@ -48,8 +48,8 @@ fn print_diff() {
 (nothing)
 
 Changed:
--pub fn example_api::function(v1_param: Struct)
-+pub fn example_api::function(v1_param: Struct, v2_param: usize)
+-pub fn example_api::function(v1_param: example_api::Struct)
++pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize)
 -pub struct example_api::Struct
 +#[non_exhaustive] pub struct example_api::Struct
 
@@ -84,8 +84,8 @@ fn print_diff_reversed() {
 Changed:
 -#[non_exhaustive] pub struct example_api::Struct
 +pub struct example_api::Struct
--pub fn example_api::function(v1_param: Struct, v2_param: usize)
-+pub fn example_api::function(v1_param: Struct)
+-pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize)
++pub fn example_api::function(v1_param: example_api::Struct)
 
 Added:
 (nothing)

public-api/tests/public-api-lib-tests.rs
@@ -34,8 +34,8 @@ fn diff_with_added_items() {
             removed: &[],
             changed: &[
                 (
-                    "pub fn example_api::function(v1_param: Struct)",
-                    "pub fn example_api::function(v1_param: Struct, v2_param: usize)",
+                    "pub fn example_api::function(v1_param: example_api::Struct)",
+                    "pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize)",
                 ),
                 (
                     "pub struct example_api::Struct",
@@ -82,8 +82,8 @@ fn diff_with_removed_items() {
                     "pub struct example_api::Struct",
                 ),
                 (
-                    "pub fn example_api::function(v1_param: Struct, v2_param: usize)",
-                    "pub fn example_api::function(v1_param: Struct)",
+                    "pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize)",
+                    "pub fn example_api::function(v1_param: example_api::Struct)",
                 ),
             ],
             added: &[],
@@ -172,8 +172,8 @@ fn pretty_printed_diff() {
     removed: [],
     changed: [
         ChangedPublicItem {
-            old: pub fn example_api::function(v1_param: Struct),
-            new: pub fn example_api::function(v1_param: Struct, v2_param: usize),
+            old: pub fn example_api::function(v1_param: example_api::Struct),
+            new: pub fn example_api::function(v1_param: example_api::Struct, v2_param: usize),
         },
         ChangedPublicItem {
             old: pub struct example_api::Struct,

test-apis/comprehensive_api/src/exports.rs
@@ -26,3 +26,38 @@ pub mod recursion_glob_1 {
 pub mod recursion_glob_2 {
     pub use super::recursion_glob_1::*;
 }
+
+/// Regression test for <https://github.com/Enselic/cargo-public-api/issues/145>
+pub mod issue_145 {
+    pub mod external {
+        pub struct External;
+    }
+
+    use external as privately_renamed;
+    use external::External;
+    use external::External as PrivatelyRenamed;
+
+    pub fn external_arg_type(_transform: External) {}
+    pub fn privately_renamed_arg_type(_transform: PrivatelyRenamed) {}
+    pub fn external_external_arg_type(_transform: external::External) {}
+    pub fn privately_renamed_external_arg_type(_transform: privately_renamed::External) {}
+
+    pub mod external_2 {
+        pub struct External;
+    }
+
+    pub use external_2 as publicly_renamed;
+    use external_2::External as PrivatelyRenamed2;
+
+    pub fn privately_renamed_2_arg_type(_transform: PrivatelyRenamed2) {}
+    pub fn publicly_renamed_external(_transform: publicly_renamed::External) {}
+
+    pub mod external_3 {
+        pub struct External;
+    }
+
+    use external_3 as privately_renamed_3;
+    pub use privately_renamed_3::External as PubliclyRenamedFromPrivateMod;
+
+    pub fn publicly_renamed_from_private_mod_arg_type(_transform: PubliclyRenamedFromPrivateMod) {}
+}

commit 3aa688ea1f1eeb07f7763d96546341023a2275fe

Always render generic args for `Path` if present
Accidental regression introduced in 4a1ad45.

public-api/src/render.rs
@@ -468,9 +468,9 @@ impl<'a> RenderingContext<'a> {
             output.extend(self.render_path(&item.path()));
         } else if !name.is_empty() {
             output.extend(self.render_path_name(name));
-            if let Some(args) = &path.args {
-                output.extend(self.render_generic_args(args));
-            }
+        }
+        if let Some(args) = &path.args {
+            output.extend(self.render_generic_args(args));
         }
         output
     }
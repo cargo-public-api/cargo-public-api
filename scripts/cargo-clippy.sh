43e19bc985e1f2bc23d74d254c24f4ab89324cae
Martin Nordholts <enselic@gmail.com>
2022-11-23 18:45:05 +0100

    scripts/cargo-clippy.sh: Deny `unsafe_code` to make auditing easier
    Inspired by https://gist.github.com/repi/d98bf9c202ec567fd67ef9e31152f43f

--- scripts/cargo-clippy.sh
+++ scripts/cargo-clippy.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
+# You can run this script from within your IDE (see `.vscode/tasks.json` for an
+# example configuration)
+
 # Applies to all packages
-cargo clippy --locked --all-targets --all-features -- --deny clippy::all --deny clippy::pedantic --deny warnings
+cargo clippy \
+    --locked \
+    --all-targets \
+    --all-features \
+    -- \
+    --deny clippy::all \
+    --deny clippy::pedantic \
+    --deny warnings \
+    --deny unsafe_code \
 
 # Only --deny missing_docs for our libs because it does not matter for bins
-cargo clippy --locked --lib --package rustdoc-json --package public-api --all-features -- --deny missing_docs
+cargo clippy \
+    --locked \
+    --lib \
+    --package rustdoc-json \
+    --package public-api \
+    --all-features \
+    -- \
+    --deny missing_docs \

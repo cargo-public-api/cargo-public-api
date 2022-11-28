ce3e81f891e6e2254412e45a9dcefa5dfb9b22e7
Martin Nordholts <enselic@gmail.com>
2022-11-28 18:46:39 +0100

    Auto-create PR with blessed output from latest nightly
    Closes #217
    
    Also change CI.yml to trigger its nightly build via Nightly.yml rather
    than by itself, to make things more neatly organized.

---
+++ scripts/auto-bless.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -o nounset -o pipefail -o errexit
+
+# Bless
+./scripts/bless.sh
+
+# If nothing changed there is nothing more to do
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Nothing to bless so nothing to do, exiting"
+    exit 0
+fi
+
+# Figure out name of current nightly
+current_nightly=$(echo nightly-$(date +%Y-%m-%d))
+
+# Prepare a branch to create a PR from
+# With `date +%s` so that repeated runs don't get the same branch name
+branch_name="auto-bless-${current_nightly}-$(date +%s)"
+git checkout -b "${branch_name}"
+
+# Commit the blessed changes and push the branch
+git config user.email "junta-pixlar0l@github.com"
+git config user.name "EnselicCICD"
+git commit -a -m "Bless output with ${current_nightly}"
+git push origin "${branch_name}"
+
+# Create a PR from the newly pushed branch
+gh pr create --head --fill --label "category-exclude"

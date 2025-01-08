135be8bfe2b1470ea27de1da1e637f8ec7e209a0
Martin Nordholts <martin.nordholts@codetale.se>
2025-01-08 12:58:58 +0100

    scripts/run-ci-locally.sh: Move config below file description

--- scripts/run-ci-locally.sh
+++ scripts/run-ci-locally.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit -o xtrace
 
-# CI sets this, so we should too
-export CARGO_TERM_COLOR=always
-
 # This script tries to emulate a run of CI.yml. If you can run this script
 # without errors you can be reasonably sure that CI will pass for real when you
 # push the code.
+
+# We set this in GitHub workflow files so we should also set it here.
+export CARGO_TERM_COLOR=always
 
 cargo fmt -- --check
 

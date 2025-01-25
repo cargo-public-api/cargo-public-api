e14d79833745c7f76e8bd042b0f015fd098d352f
Martin Nordholts <martin.nordholts@codetale.se>
2025-01-25 16:26:09 +0100

    public-api: Support for `nightly-2025-01-25`

--- scripts/release-helper/src/version_info.rs
+++ scripts/release-helper/src/version_info.rs
@@ -7,6 +7,10 @@ pub struct CargoPublicApiVersionInfo {
 
 pub static TABLE: &[CargoPublicApiVersionInfo] = &[
     CargoPublicApiVersionInfo {
+        cargo_public_api_version: "0.43.x",
+        min_nightly_rust_version: "nightly-2025-01-25",
+    },
+    CargoPublicApiVersionInfo {
         cargo_public_api_version: "0.42.x",
         min_nightly_rust_version: "nightly-2024-10-18",
     },

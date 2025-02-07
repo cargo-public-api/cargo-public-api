106201b9cc1bea6261220eb3e9be624c00d4291b
Martin Nordholts <martin.nordholts@codetale.se>
2025-02-07 16:45:21 +0100

    scripts/cargo-test.sh: Remove obsolete export UPDATE_EXPECT=1
    It was forgotten in d3b92b82c0f2.

--- scripts/cargo-test.sh
+++ scripts/cargo-test.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
-# Make it easy to bless changed output. Simply run this to bless:
-#
-#   ./scripts/cargo-test.sh --bless
-if [ "${1:-}" = "--bless" ]; then
-    export UPDATE_EXPECT=1
-fi
-
 # Since `std::env::set_var()` is unsafe in Rust Edition 2024 we don't even
 # try to modify `PATH` inside of tests. Instead we make sure that it is set
 # appropriately from the start. Since we don't pass `--release` to the below

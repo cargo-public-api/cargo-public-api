16e905611d9cbe1c728fb6dad94ca591d64c8fee
Martin Nordholts <martin.nordholts@codetale.se>
2025-02-10 18:34:19 +0100

    scripts/cargo-test.sh: Clarify why we modify PATH

--- scripts/cargo-test.sh
+++ scripts/cargo-test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
+# Put `cargo-public-api` in $PATH so `cargo` finds it and `cargo public-api`
+# works.
+#
 # Since `std::env::set_var()` is unsafe in Rust Edition 2024 we don't even
 # try to modify `PATH` inside of tests. Instead we make sure that it is set
 # appropriately from the start. Since we don't pass `--release` to the below

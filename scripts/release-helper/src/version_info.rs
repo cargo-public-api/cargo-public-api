b8773c6510fdbf64740951ff10e6bc45dce0ac85
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-28 06:59:02 +0100

    Bump rustdoc-types to 0.38.0 and cargo-public-api to 0.46.0
    This version will likely never be released, but by leaving a "whole" we
    can do it retroactively if we find a reason to.
    
    Tests do not pass in this commit.

--- scripts/release-helper/src/version_info.rs
+++ scripts/release-helper/src/version_info.rs
@@ -7,7 +7,11 @@ pub struct CargoPublicApiVersionInfo {
 
 pub static TABLE: &[CargoPublicApiVersionInfo] = &[
     CargoPublicApiVersionInfo {
-        cargo_public_api_version: "0.45.x",
+        cargo_public_api_version: "0.46.x",
+        min_nightly_rust_version: "nightly-2025-03-16",
+    },
+    CargoPublicApiVersionInfo {
+        cargo_public_api_version: "0.45.x", // was never actually released
         min_nightly_rust_version: "nightly-2025-03-14",
     },
     CargoPublicApiVersionInfo {
